aef99629b45ea3d7ac3c24584a10bc3fa0616a7b
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-04-05 15:24:07 -0700

    Convert GET requests to async

--- MarkedDown/API/Controllers/ProductsController.cs
+++ MarkedDown/API/Controllers/ProductsController.cs
@@ -1,8 +1,10 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -17,17 +19,17 @@ namespace API.Controllers
     }
 
     [HttpGet]
-    public ActionResult<List<Product>> GetProducts()
+    public async Task<ActionResult<List<Product>>> GetProducts()
     {
-      var products = _context.Products.ToList();
+      var products = await _context.Products.ToListAsync();
 
       return Ok(products);
     }
 
     [HttpGet("{id}")]
-    public string GetProduct(int id)
+    public async Task<ActionResult<Product>> GetProduct(int id)
     {
-      return "This will be a single product.";
+      return await _context.Products.FindAsync(id);
     }
   }
 }

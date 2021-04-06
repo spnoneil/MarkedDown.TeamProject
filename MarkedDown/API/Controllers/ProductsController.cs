679da2d8300d715536b8e906aa21d38e513951bc
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-04-05 23:54:59 -0700

    Update cproduct controller to use specification methods

--- MarkedDown/API/Controllers/ProductsController.cs
+++ MarkedDown/API/Controllers/ProductsController.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Core.Entities;
 using Core.Interfaces;
+using Core.Specifications;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -25,7 +26,9 @@ namespace API.Controllers
     [HttpGet]
     public async Task<ActionResult<List<Product>>> GetProducts()
     {
-      var products = await _productsRepo.ListAllAsync();
+      var spec = new ProductsWithTypesAndBrandsSpecification();
+
+      var products = await _productsRepo.ListAsync(spec);
 
       return Ok(products);
     }
@@ -33,7 +36,9 @@ namespace API.Controllers
     [HttpGet("{id}")]
     public async Task<ActionResult<Product>> GetProduct(int id)
     {
-      return await _productsRepo.GetByIdAsync(id);
+      var spec = new ProductsWithTypesAndBrandsSpecification(id);
+
+      return await _productsRepo.GetEntityWithSpec(spec);
     }
 
     [HttpGet("brands")]

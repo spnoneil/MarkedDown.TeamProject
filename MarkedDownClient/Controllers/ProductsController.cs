c5b653efba73bdc4fa14443269320779f14d90af
Scott O'Neil <scottieoneil@gmail.com>
2021-04-06 13:31:24 -0700

    add product model / controller
    Co-authored-by: louiesch <louiesch@users.noreply.github.com>

---
+++ MarkedDownClient/Controllers/ProductsController.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MarkedDownClient.Models;
+
+namespace MarkedDownClient.Controllers
+{
+  public class ProductsController : Controller
+  {
+    public IActionResult Index()
+    {
+      var allProducts = Product.GetProducts();
+        return View(allProducts);
+    }
+
+    [HttpPost]
+    public IActionResult Index(Product product)
+    {
+      Product.Post(product);
+      return RedirectToAction("Index");
+    }
+
+    public IActionResult Details(int id)
+    {
+      var product = Product.GetDetails(id);
+      return View(product);
+    }
+
+    public IActionResult Edit(int id)
+    {
+      var product = Product.GetDetails(id);
+      return View(product);
+    }
+
+    [HttpPost]
+    public IActionResult Details(int id, Product product)
+    {
+      product.ProductId = id;
+      Product.Put(product);
+      return RedirectToAction("Details", id);
+    }
+
+    public IActionResult Delete(int id)
+    {
+      Product.Delete(id);
+      return RedirectToAction("Index");
+    }
+  }
+}
---
+++ MarkedDownClient/Models/Product.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+
+namespace Core.Entities
+{
+    public class Product : BaseEntity
+    {
+        // public int ProductId { get; set; }
+      public string Name { get; set; }
+      public string Description { get; set; }
+      public decimal Price { get; set; }
+
+      public string PictureUrl { get; set; }
+      public ProductType ProductType { get; set; }
+      public int ProductTypeId { get; set; }
+      public ProductBrand ProductBrand { get; set; }
+      public int ProductBrandId { get; set; }
+
+      public static List<Product> GetProducts()
+      {
+
+      }
+    }
+}
+
+

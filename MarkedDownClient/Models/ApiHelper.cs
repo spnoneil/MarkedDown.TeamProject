41d867f90a4ab9fa01006587b3b8b8f999ce37b4
Scott O'Neil <scottieoneil@gmail.com>
2021-04-06 15:38:02 -0700

    add get type / brand to apihelper

--- MarkedDownClient/Models/ApiHelper.cs
+++ MarkedDownClient/Models/ApiHelper.cs
@@ -14,8 +14,24 @@ namespace MarkedDownClient.Models
     }
     public static async Task<string> Get(int id)
     {
-      RestClient client = new RestClient("http://localhost:5000/api");
+      RestClient client = new RestClient("http://localhost:5001/api");
       RestRequest request = new RestRequest($"products/{id}", Method.GET);
+      var response = await client.ExecuteTaskAsync(request);
+      return response.Content;
+    }
+
+    public static async Task<string> GetBrand(string brand)
+    {
+      RestClient client = new RestClient("http://localhost:5001/api");
+      RestRequest request = new RestRequest($"products/{brand}", Method.GET);
+      var response = await client.ExecuteTaskAsync(request);
+      return response.Content;
+    }
+
+    public static async Task<string> GetType(string type)
+    {
+      RestClient client = new RestClient("http://localhost:5001/api");
+      RestRequest request = new RestRequest($"products/{type}", Method.GET);
       var response = await client.ExecuteTaskAsync(request);
       return response.Content;
     }

7d202f2c73e94ce412af93f7b649791bd69bc49f
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-04-05 19:47:27 -0700

    Update migrations

--- MarkedDown/Infrastructure/Data/Migrations/20210406023642_Initial.Designer.cs
+++ MarkedDown/Infrastructure/Data/Migrations/20210406023642_Initial.Designer.cs
@@ -2,14 +2,16 @@
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Infrastructure.Data.Migrations
 {
     [DbContext(typeof(StoreContext))]
-    partial class StoreContextModelSnapshot : ModelSnapshot
+    [Migration("20210406023642_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

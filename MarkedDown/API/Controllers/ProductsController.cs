135ed6ceca768c93c9ec017272bd9ef38dcc1a99
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-04-06 13:41:38 -0700

    Switch to base api controller, add attributes to display error endpoints for swagger

--- MarkedDown/API/Controllers/ProductsController.cs
+++ MarkedDown/API/Controllers/ProductsController.cs
@@ -1,18 +1,17 @@
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using API.DTOs;
+using API.Errors;
 using AutoMapper;
 using Core.Entities;
 using Core.Interfaces;
 using Core.Specifications;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-  [ApiController]
-  [Route("api/[controller]")]
-  public class ProductsController : ControllerBase
+  public class ProductsController : BaseApiController
   {
     private readonly IGenericRepository<Product> _productsRepo;
     private readonly IGenericRepository<ProductBrand> _productBrand;
@@ -29,6 +28,8 @@ namespace API.Controllers
     }
 
     [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
     public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts()
     {
       var spec = new ProductsWithTypesAndBrandsSpecification();
@@ -39,22 +40,33 @@ namespace API.Controllers
     }
 
     [HttpGet("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
     public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
     {
       var spec = new ProductsWithTypesAndBrandsSpecification(id);
 
       var product = await _productsRepo.GetEntityWithSpec(spec);
 
+      if (product == null) 
+      {
+        return NotFound(new ApiResponse(404));
+      }
+
       return _mapper.Map<Product, ProductToReturnDto>(product);
     }
 
     [HttpGet("brands")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
     public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
     {
       return Ok(await _productBrandRepo.ListAllAsync());
     }
 
     [HttpGet("types")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
     public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
     {
       return Ok(await _productTypeRepo.ListAllAsync());

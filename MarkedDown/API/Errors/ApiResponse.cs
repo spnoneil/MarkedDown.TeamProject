6473581f100399d6ed64fd80c47f0442cc9b588b
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-04-06 13:48:13 -0700

    Add Api response class to return custom/uniform error messages

---
+++ MarkedDown/API/Errors/ApiResponse.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace API.Errors
+{
+  public class ApiResponse
+  {
+    public ApiResponse(int statusCode, string message = null)
+    {
+      StatusCode = statusCode;
+      Message = message ?? GetDefaultMessageForStatusCode(statusCode);
+    }
+    
+    public int StatusCode { get; set; }
+    public string Message { get; set; }
+
+    private string GetDefaultMessageForStatusCode(int statusCode)
+    {
+      return statusCode switch
+      {
+        400 => "Bad request",
+        401 => "Not authorized",
+        404 => "Resource not found",
+        500 => "Error",
+        _ => null
+      };
+    }
+
+  }
+}

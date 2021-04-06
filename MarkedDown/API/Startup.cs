b5bcaa8703df5908da30d37256eb4df1589adcc1
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-04-06 00:56:39 -0700

    Add mapper services, app.UseStaticFiles, remove unused directives

--- MarkedDown/API/Startup.cs
+++ MarkedDown/API/Startup.cs
@@ -1,18 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using API.Helpers;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 namespace API
@@ -30,6 +24,8 @@ namespace API
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
+      services.AddAutoMapper(typeof(MappingProfiles));
+
       services.AddScoped<IProductRepository, ProductRepository>();
       
       services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
@@ -58,6 +54,8 @@ namespace API
       app.UseHttpsRedirection();
 
       app.UseRouting();
+
+      app.UseStaticFiles();
 
       app.UseAuthorization();
 

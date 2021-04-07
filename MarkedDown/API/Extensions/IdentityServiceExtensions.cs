f45d2e46dcfc88638caa0acb4b0facc2b392550d
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-04-07 10:34:14 -0700

    Add identity extension

---
+++ MarkedDown/API/Extensions/IdentityServiceExtensions.cs
@@ -0,0 +1,24 @@
+using Core.Entities.Identity;
+using Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace API.Extensions
+{
+  public static class IdentityServiceExtensions
+  {
+    public static IServiceCollection AddIdentityServices(this IServiceCollection services)
+    {
+      var builder = services.AddIdentityCore<AppUser>();
+
+      builder = new IdentityBuilder(builder.UserType, builder.Services);
+      builder.AddEntityFrameworkStores<AppIdentityDbContext>();
+      builder.AddSignInManager<SignInManager<AppUser>>();
+
+      // temporary to prevent errors when using sign in manger 
+      services.AddAuthentication();
+
+      return services;
+    }
+  }
+}

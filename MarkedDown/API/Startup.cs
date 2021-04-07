a3fe0a353feee7b6ca66c5ba5e80ab0b7d060e8a
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-04-07 01:12:08 -0700

    Fix typo

--- MarkedDown/API/Startup.cs
+++ MarkedDown/API/Startup.cs
@@ -31,7 +31,7 @@ namespace API
       services.AddDbContext<StoreContext>(x =>
           x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
 
-      services.AddSingleton<ConnectionMultiplexer>(c => {
+      services.AddSingleton<IConnectionMultiplexer>(c => {
         var configuration = ConfigurationOptions.Parse(_config.GetConnectionString("Redis"), true);
         return ConnectionMultiplexer.Connect(configuration);
       });

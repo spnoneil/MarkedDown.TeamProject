245b4604c2a9c77c936c5cebe7bd0f67e45ebf4f
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-04-07 09:32:43 -0700

    Add service and connection string for identity db

--- MarkedDown/API/Startup.cs
+++ MarkedDown/API/Startup.cs
@@ -2,6 +2,7 @@ using API.Extensions;
 using API.Helpers;
 using API.Middleware;
 using Infrastructure.Data;
+using Infrastructure.Identity;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -30,6 +31,12 @@ namespace API
 
       services.AddDbContext<StoreContext>(x =>
           x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+
+      // separate database for identity to create boundary between application database and identity database
+      services.AddDbContext<AppIdentityDbContext>(x =>
+      {
+        x.UseSqlite(_config.GetConnectionString("IdentityConnection"));
+      });
 
       services.AddSingleton<IConnectionMultiplexer>(c => {
         var configuration = ConfigurationOptions.Parse(_config.GetConnectionString("Redis"), true);
